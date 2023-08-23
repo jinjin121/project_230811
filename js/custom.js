fd3c2e12a6194bf66dfc51ede303bb992595d803
jinjin121 <110872022+jinjin121@users.noreply.github.com>
2023-08-24 08:18:13 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,34 @@ $(function () {
     $('.slide01').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
         speed: 1000,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
-    $('.slide01_slick_btn .play_btn a img').click(function () {
+    $('.slide01_slick_btn .play_btn').click(function () {
         $('.slide01').slick('slickPlay');
     });
-    $('.slide01_slick_btn .stop_btn a img').click(function () {
+    $('.slide01_slick_btn .stop_btn').click(function () {
         $('.slide01').slick('slickPause');
     });
-
+    $('.slide02').slick({
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+    $('.slide03').slick({
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
 
 });
